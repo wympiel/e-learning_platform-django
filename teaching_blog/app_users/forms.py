5b6c47c9128ead2e9fd0a7130f7f34905586d988
wympiel <wymp10@gmial.com>
2021-04-19 19:00:46 +0200

    first commit

---
+++ teaching_blog/app_users/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from app_users.models import UserProfileInfo
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+        labels = {
+            'password1': 'Hasło',
+            'password2': 'Potwierdź hasło'
+        }
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    bio = forms.CharField(required=False)
+    teacher = 'trener'
+    student = 'student'
+    parent = 'rodzic'
+    user_types = [
+        (student, 'student'),
+        (parent, 'rodzic')
+    ]
+    user_type = forms.ChoiceField(required=True, choices=user_types)
+
+    class Meta():
+        model = UserProfileInfo
+        fields = ('bio', 'profile_pic', 'user_type')
---
+++ teaching_blog/app_users/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+    registered = False
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'app_users/registration.html',
+                  {'registered': registered,
+                   'user_form': user_form,
+                   'profile_form': profile_form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('KONTO JEST NIEAKTYWNE')
+        else:
+            return HttpResponse('Proszę podaj prawidłowe id i hasło')
+    else:
+        return render(request, 'app_users/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
